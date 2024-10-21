740249fdf6ba8f433d0edd0c1156747fc63321b6
a_reyes <a_reyes@penguin>
2024-10-21 11:35:36 -0400

    Output a string of characters using puts()

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * This program prints a string of characters using puts()
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
