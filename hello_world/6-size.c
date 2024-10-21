b1b676cf9d8e92d79fdd0a27e4b8808faabc8294
a_reyes <a_reyes@penguin>
2024-10-21 13:21:51 -0400

    Return the sizeof input

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-
-int main(int argc, char *argv)
+/**
+ * main - Entry point of the program
+ * @argc: First argument
+ * @argv: Second argument
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
 {
+	printf("%lu", sizeof(argv));
 	return (0);
 }
