48687284e2b1e189f7f410884dc483f82400c79c
a_reyes <a_reyes@penguin>
2024-10-22 14:40:38 -0400

    Check for negative or positive

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - Computes the absolute value of an integer
+ * @n: parameter to be evaluated
+ * Return: The absolute value of an input
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	return (n);
+}
