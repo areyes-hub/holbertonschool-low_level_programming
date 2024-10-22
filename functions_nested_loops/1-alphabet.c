344512968b0b550dcfb220b8c98d6efcc700bd22
a_reyes <a_reyes@penguin>
2024-10-22 10:24:13 -0400

    Print alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,15 +1,5 @@
 #include "main.h"
 /**
- * main - Entry point
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-/**
  * print_alphabet - Print all characters
  * Return: void
  */
