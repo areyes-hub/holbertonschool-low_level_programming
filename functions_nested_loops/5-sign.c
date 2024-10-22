6eca5b884851b05ee89b650e8e61cbe8dfc48c1b
a_reyes <a_reyes@penguin>
2024-10-22 13:32:27 -0400

    Check for negative or positive

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -6,20 +6,20 @@
  */
 int print_sign(int n)
 {
-	if (n > 48)
+	if (n == 48)
 	{
-		_putchar(43);
-		return (1);
+		_putchar(48);
+		return (0);
 	}
 	else if (n < 48)
 	{
 		_putchar(45);
 		return (-1);
 	}
-	else if (n == 0)
+	else if (n > 48)
 	{
-		_putchar(48);
-		return (0);
+		_putchar(43);
+		return (1);
 	}
 	return (0);
 }
