46c83ff9d8e3e5b3e4552282c433af94109f7706
a_reyes <a_reyes@penguin>
2024-10-23 18:28:39 -0400

    Print fibonacci sequence

---
+++ functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: first fifty nums of the fibonacci sequence
+ */
+int main(void)
+{
+	int i, n = 50, n1 = 0, n2 = 1, next;
+
+	printf("%d", n2);
+	for (i = 1; i < n; i++)
+	{
+		next = n1 + n2;
+
+		printf(", ");
+		printf("%d", next);
+		n1 = n2;
+		n2 = next;
+	}
+	printf("\n");
+	return (0);
+}
