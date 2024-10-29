70fbdbf0822367e832555cd76d7b461f7f65e246
a_reyes <a_reyes@penguin>
2024-10-29 10:53:01 -0400

    Reverses a string

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,6 +7,7 @@
 void rev_string(char *s)
 {
 	int i = 0, length;
+
 	length = _strlen(s) - 1;
 	while (length > i)
 	{
@@ -23,6 +24,7 @@ void rev_string(char *s)
 int _strlen(char *s)
 {
 	int count, inc;
+
 	inc = 0;
 	for (count = 0; s[count] != '\0'; count++)
 		inc++;
@@ -38,6 +40,7 @@ int _strlen(char *s)
 void swap_char(char *a, char *b)
 {
 	char tmp = *a;
+
 	*a = *b;
 	*b = tmp;
 }
