be4af5fbe79e17ea37da86ad90a698c39976fb19
a_reyes <a_reyes@penguin>
2024-10-29 13:47:46 -0400

    Prints n numbers of an array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - Prints n elements of an array of integers
+ * @a: first parameter
+ * @n: second parameter
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[1]);
+	}
+}
