ec527d501c6b8d44b23955fabe56f3f895e8d2c0
a_reyes <a_reyes@penguin>
2024-10-29 14:00:38 -0400

    Prints n numbers of an array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -9,9 +9,24 @@
 void print_array(int *a, int n)
 {
 	int i;
+	int len = 0;
+	int real_len;
+	int item = 0;
 
+	while (a[item] != '\0')
+	{
+		len++;
+		item++;
+		real_len = len - 1;
+	}
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", a[1]);
+		printf("%d", a[i]);
+		if (i == real_len)
+		{
+			break;
+		}
+		printf(", ");
 	}
+	printf("\n");
 }
