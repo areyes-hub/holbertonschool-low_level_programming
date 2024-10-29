4a83e4efec2539ddab61bdd491aca69b26300919
a_reyes <a_reyes@penguin>
2024-10-29 09:24:11 -0400

    Prints a reversed string

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - Prints a reversed string
+ * @s: Parameter to evaluate
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+	int item = 0;
+	int i;
+
+	while (s[item] != '\0')
+	{
+		len++;
+		item++;
+	}
+	for (i = len; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+}
