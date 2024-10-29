a9eda138d429beed1530b1cd047a7deba3c0ed6a
a_reyes <a_reyes@penguin>
2024-10-29 12:04:43 -0400

    Prints half the characters of a string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half - Prints half of a string
+ * @str: Parameter to evaluate
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	int item = 0;
+	int half;
+	int i;
+
+	while (str[item] != '\0')
+	{
+		len++;
+		item++;
+		half = len / 2;
+	}
+	for (i = half; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
