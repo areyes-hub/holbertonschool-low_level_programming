c110141766be78e79ff817e88ed635346e8807b2
a_reyes <a_reyes@penguin>
2024-10-30 18:20:41 -0400

    Capitalize all words of a string

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -8,12 +8,28 @@ char *cap_string(char *s)
 {
 	int item = 0;
 
-	while (s[item] != '\0')
+	while (s[item])
 	{
-		if ((s[item] == ' ' || s[item] == '\n' || s[item] == '\t' ||
-					s[item] == '.') && (s[item + 1] > '`' && s[item + 1] < '{'))
+		while (!(s[item] >= 'a' && s[item] <= 'z'))
 		{
-			s[item + 1] = s[item + 1] - 32;
+			item++;
+		}
+		if (s[item - 1] == ' ' ||
+		    s[item - 1] == '\t' ||
+		    s[item - 1] == '\n' ||
+		    s[item - 1] == ',' ||
+		    s[item - 1] == ';' ||
+		    s[item - 1] == '.' ||
+		    s[item - 1] == '!' ||
+		    s[item - 1] == '?' ||
+		    s[item - 1] == '"' ||
+		    s[item - 1] == '(' ||
+		    s[item - 1] == ')' ||
+		    s[item - 1] == '{' ||
+		    s[item - 1] == '}' ||
+		    item == 0)
+		{
+			s[item] -= 32;
 		}
 		item++;
 	}
