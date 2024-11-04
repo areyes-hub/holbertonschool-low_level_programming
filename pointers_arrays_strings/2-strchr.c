1852775af1afd6b406d32a0a2226feb54a26fd98
a_reyes <a_reyes@penguin>
2024-11-04 11:30:38 -0400

    Locates the first instance of a character

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - Locates a character in a string
+ * @s: First parameter
+ * @c: Second parameter
+ * Return: A pointer to the first occurrance of the character, NULL otherwise
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+	int j = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		j += i;
+		if (s[i] == c)
+		{
+			return ((s + j) - 1);
+		}
+	}
+	return (NULL);
+}
