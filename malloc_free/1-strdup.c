4e766bdb1d35fd4e6a7082e03a2a29977a70d25f
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2024-11-11 11:38:32 -0400

    Duplicates a string in a newly allocated memory space

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - Points to a string copy in a new memory allocation
+ * @str: The string to duplicate
+ * Return: On success, a pointer to the dup string, else NULL
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	int i;
+
+	dup = malloc(sizeof(str));
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		dup[i] = str[i];
+	}
+	dup[i] = '\0';
+	return (dup);
+}
