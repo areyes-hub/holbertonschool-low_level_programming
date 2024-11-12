2bc16564af0387ecc2ef1703b2535cf31af0bff6
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2024-11-12 15:24:11 -0400

    Create a dog struct

---
+++ structures_typedef/README.md
@@ -0,0 +1,2 @@
+#C - Structures, Typedef
+Here you will find files and excercises related to structures and typedef in c.
---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * struct dog - Creates a dog structure
+ * @name: name of the dog
+ * @age: dogs age
+ * @owner: dogs mom or dad
+ * Description: This struct stores about our friend
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
