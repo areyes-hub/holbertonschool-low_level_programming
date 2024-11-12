b21100baea2897bab2cc80b83396d0b81eef36f5
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2024-11-12 16:45:41 -0400

    Print a struct

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -10,6 +10,18 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
+		if (!d->name)
+		{
+			printf("Name: (nil)\nAge: %f\nOwner: %s\n", d->age, d->owner);
+		}
+		else if (!d->age)
+		{
+			printf("Name: %s\nAge: (nil)\nOwner: %s\n", d->name, d->owner);
+		}
+		else if (!d->owner)
+		{
+			printf("Name: %s\nAge: %f\nOwner: (nil)\n", d->name, d->age);
+		}
 		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 	}
 }
