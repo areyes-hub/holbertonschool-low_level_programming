017212c9e386b7e4896997eb9f598a0c74252b3e
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2024-11-12 23:14:40 -0400

    Create a new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -10,21 +10,41 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *doggo;
-	dog_t *perro;
+	dog_t *d;
+	int len;
+	char *ptr;
 
 	if (name == 0 || owner == 0)
+		return (NULL);
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
+
+	for (len = 1, ptr = name; *ptr; len++)
+		ptr++;
+	d->name = malloc(len);
+	if (d->name == 0)
 	{
+		free(d);
 		return (NULL);
 	}
-	doggo = malloc(sizeof(dog_t));
-	perro = doggo;
-	if (doggo == NULL)
+
+	for (len = 1, ptr = owner; *ptr; len++)
+		ptr++;
+	d->owner = malloc(len);
+	if (d->owner == 0)
 	{
+		free(d->name);
+		free(d);
 		return (NULL);
 	}
-	doggo->name = name;
-	doggo->age = age;
-	doggo->owner = owner;
-	return (perro);
+	for (len = 0; *name != 0; len++, name++)
+		d->name[len] = *name;
+	d->name[len] = 0;
+	for (len = 0; *owner != 0; len++)
+		d->owner[len] = *owner++;
+	d->owner[len] = 0;
+	d->age = age;
+
+	return (d);
 }
