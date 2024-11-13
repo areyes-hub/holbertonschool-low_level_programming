d6722db35ae243dadcc3fad5f26413e64450b85d
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2024-11-12 22:53:30 -0400

    Create a new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,7 +12,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *doggo;
 
-	doggo = malloc(sizeof(dog_t));
+	doggo = malloc(sizeof(struct dog));
 	if (doggo == NULL)
 	{
 		return (NULL);
