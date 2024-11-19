0fc82450a29eb6fbcf891c85094ef60ca63323ff
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2024-11-18 20:50:44 -0400

    Perform simple operations

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -20,6 +20,11 @@ int (*get_op_func(char *s))(int, int)
 
 	while (ops[i].op != NULL)
 	{
+		if (*s != *(ops[i].op))
+		{
+			printf("Error\n");
+			exit(99);
+		}
 		if (*s == *(ops[i].op))
 		{
 			return (ops[i].f);
