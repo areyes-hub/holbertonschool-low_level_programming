4700fa54b8a06bfc2e376b1f30e5bff7bcb6e5e1
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2024-11-18 20:45:45 -0400

    Perform simple operations

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -18,12 +18,13 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i;
 
-	for (i = 0; ops[i].op != NULL; i++)
+	while (ops[i].op != NULL)
 	{
 		if (*s == *(ops[i].op))
 		{
 			return (ops[i].f);
 		}
+		i++;
 	}
 	return (NULL);
 }
