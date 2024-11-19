bedd67042cbdc914188e895389e2590100a29f36
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2024-11-19 15:39:42 -0400

    Print numbers

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -22,7 +22,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("%d", arg);
 		if (i < n - 1 && separator)
 		{
-			printf("%c ", *separator);
+			printf("%s ", separator);
 		}
 	}
 	printf("\n");
