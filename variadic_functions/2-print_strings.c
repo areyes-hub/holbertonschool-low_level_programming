a09adf2a6848d368a6dc4f518450510e7d13847c
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2024-11-19 10:33:55 -0400

    Print all strings

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+/**
+ * print_strings - prints strings given
+ * @separator: character to print between strings
+ * @n: number of arguments
+ * @...: parameters
+ * Return: void.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	char *arg;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		arg = va_arg(args, char*);
+		printf("%s", arg);
+		if (arg == NULL)
+		{
+			printf("(nil)");
+		}
+		if (i == n - 1)
+		{
+			break;
+		}
+		printf("%c ", *separator);
+	}
+	printf("\n");
+}
